b2f2fb25af99e17be5175df8eb0aa92a2384ceb9
Karl Petter Lühr Brevik <kpbrevik@stud.ntnu.no>
2025-03-21 12:09:34 +0100

    Commit meldninga mi, commit nr 3 Microkontroll opp til 2.5

---
+++ lab_3-0.0.16/2_uart/main.c
@@ -0,0 +1,83 @@
+#include "uart.h"
+#include <stdint.h>
+
+#include <stdint.h>
+
+//BUTTONS1,4 = P0.13,16
+#define __BUTTON_1_PIN__ 13
+#define __BUTTON_2_PIN__ 14
+#define __BUTTON_3_PIN__ 15
+#define __BUTTON_4_PIN__ 16
+
+
+//__GPIO_BASE_ADRESS0__ = P0, 0x50000000
+#define __GPIO_BASE_ADDRESS__ 0x50000000
+
+//0x700 - 0x524 = 0x1DC = 476 bytes = 119
+#define __RESERVED1_SIZE__ 118
+
+#define GPIO ((NRF_GPIO_REGS*)__GPIO_BASE_ADDRESS__)
+
+typedef struct {
+	volatile uint32_t RESERVED0[321];
+	volatile uint32_t OUT;
+	volatile uint32_t OUTSET;
+	volatile uint32_t OUTCLR;
+	volatile uint32_t IN;
+	volatile uint32_t DIR;
+	volatile uint32_t DIRSET;
+	volatile uint32_t DIRCLR;
+	volatile uint32_t LATCH;
+	volatile uint32_t DETECTMODE;
+	volatile uint32_t RESERVED1[__RESERVED1_SIZE__];
+	volatile uint32_t PIN_CNF[32];
+} NRF_GPIO_REGS;
+
+void button_init(){ 
+	// Chat seier (2 << 2) er riktig for å Enable pull-upp for BUTTON X
+	// Flytter 11 til bitene 2 thru 4
+	//PIN CNF only allows values 00,01,10
+	// (2 << 2) enables pullupp / 0b1000
+	GPIO->PIN_CNF[__BUTTON_1_PIN__] = (2 << 2);
+	// Fill inn the configuration for the remaining buttons
+	GPIO->PIN_CNF[__BUTTON_2_PIN__] = (2 << 2);
+	GPIO->PIN_CNF[__BUTTON_3_PIN__] = (2 << 2);
+	GPIO->PIN_CNF[__BUTTON_4_PIN__] = (2 << 2);
+}
+
+int main () {
+
+button_init();
+
+int button1_pressed = !(GPIO->IN & (1 << __BUTTON_1_PIN__));
+int button2_pressed = !(GPIO->IN & (1 << __BUTTON_2_PIN__));
+
+
+while(1){
+if(button1_pressed){
+	uart_send('A');
+}
+else if(button2_pressed){
+	uart_send('B');
+}
+
+
+if (uart_read() != '\0'){
+	if(	GPIO->OUTCLR == (0b1111 << 17)){
+		GPIO->OUTSET = (0b1111 << 17);
+	}
+	else if (GPIO->OUTSET == (0b1111 << 17)){
+		GPIO->OUTCLR = (0b1111 << 17);
+	}
+}}
+
+void uart_send_str(char ** str){
+	UART->TASKS_STARTTX = 1;
+	char * letter_ptr = *str;
+	while(*letter_ptr != '\0'){
+		UART->TXD = *letter_ptr;
+		while(!UART->EVENTS_TXDRDY);
+		UART->EVENTS_TXDRDY = 0;
+		letter_ptr++;
+}
+}
---
+++ lab_3-0.0.16/2_uart/uart.c
@@ -0,0 +1,83 @@
+#include "uart.h"
+#include "gpio.h"
+#include <stdint.h>
+
+#define UART_BASE 0x40002000
+#define UART ((NRF_UART_REG*)UART_BASE)
+
+typedef struct {
+    volatile uint32_t TASKS_STARTRX;
+    volatile uint32_t TASKS_STOPRX;
+    volatile uint32_t TASKS_STARTTX;
+    volatile uint32_t TASKS_STOPTX;
+    volatile uint32_t RESERVED0[3];
+    volatile uint32_t TASKS_SUSPEND;
+    volatile uint32_t RESERVED1[56];
+    volatile uint32_t EVENTS_CTS;
+    volatile uint32_t EVENTS_NCTS;
+    volatile uint32_t EVENTS_RXDRDY;
+    volatile uint32_t RESERVED2[4];
+    volatile uint32_t EVENTS_TXDRDY;
+    volatile uint32_t RESERVED3;
+    volatile uint32_t EVENTS_ERROR;
+    volatile uint32_t RESERVED4[7];
+    volatile uint32_t EVENTS_RXTO;
+    volatile uint32_t RESERVED5[46];
+    volatile uint32_t ENABLE;
+    volatile uint32_t RESERVED6;
+    volatile uint32_t PSELRTS;
+    volatile uint32_t PSELTXD;
+    volatile uint32_t PSELCTS;
+    volatile uint32_t PSELRXD;
+    volatile uint32_t RXD;
+    volatile uint32_t TXD;
+    volatile uint32_t RESERVED7;
+    volatile uint32_t BAUDRATE;
+    volatile uint32_t RESERVED8[17];
+    volatile uint32_t CONFIG;
+} NRF_UART_REG;
+
+#define UART_TX_PIN 6
+#define UART_RX_PIN 8
+
+void uart_init() {
+    // Konfigurer GPIO for UART TX (output) og UART RX (input)
+    GPIO->PIN_CNF[UART_TX_PIN] = (1 << 0); // Output
+    GPIO->PIN_CNF[UART_RX_PIN] = (0 << 0); // Input
+
+    // Koble GPIO-pinnene til UART-modulen
+    UART->PSELTXD = UART_TX_PIN;
+    UART->PSELRXD = UART_RX_PIN;
+
+    // Deaktiver RTS og CTS (flow control)
+    UART->PSELRTS = 0xFFFFFFFF;
+    UART->PSELCTS = 0xFFFFFFFF;
+
+    // Sett baudrate til 9600
+    UART->BAUDRATE = 9600; //0x00275000
+
+    // Aktiver UART
+    UART->ENABLE = 8; //previously set to 4
+
+    // Start mottak
+    UART->TASKS_STARTRX = 1;
+}
+
+void uart_send(char letter){
+	UART->TXD = letter;
+	UART->TASKS_STARTTX = 1;
+	while(!UART->EVENTS_TXDRDY);
+
+	UART->EVENTS_TXDRDY = 0;
+	UART->TASKS_STOPTX = 1;
+}
+
+char uart_read(){
+
+	UART->EVENTS_RXDRDY = 0;
+	if (UART->RXD == 0){
+		return '\0';
+	}
+
+	return UART->RXD;
+}
